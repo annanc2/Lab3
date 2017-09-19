d7674416f8fc77fd322af72ffa489635ad1e8438
annachlopecki <annachlopecki@wirelessprv-10-194-127-126.near.illinois.edu>
2017-09-19 12:50:09 -0500

    This is my first commit.

---
+++ src/main/java/lab3.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class lab3 {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+
+    public static void main(String[] args) {
+        String hamletText = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        int wordCounter = 0;
+        int indexOfLetter = 0;
+        for (int i = indexOfLetter; i < hamletText.length(); i++) {
+            if (hamletText.charAt(i) == ' ') {
+                wordCounter++;
+            }
+        }
+        System.out.print(wordCounter);
+    }
+
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+}
